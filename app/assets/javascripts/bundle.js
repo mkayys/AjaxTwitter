540ed90eb854a956737e3d2cf9c57c89a6aecb7f
Michelle Kim <x1michelleskim@gmail.com>
2019-08-22 11:22:31 -0700

    Disable button while ajax call is being made

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -135,11 +135,19 @@ class FollowToggle {
     }
 
     render() {
+
         if (this.followState === 'unfollowed') {
             this.$el.text('Follow!');
-
-        } else {
+            this.$el.removeProp('disabled');
+        } else if (this.followState === "followed") {
             this.$el.text('Unfollow!');
+            this.$el.removeProp('disabled');
+        } else if (this.followState === "following") {
+            this.$el.text('following');
+            this.$el.prop('disabled', true)
+        } else if (this.followState === "unfollowing") {
+            this.$el.text('unfollowing');
+            this.$el.prop('disabled', true);
         }
     }
 
@@ -149,14 +157,17 @@ class FollowToggle {
         e.preventDefault();
         let method = (this.followState === "followed") ? 'DELETE' : 'POST';
 
+        this.followState = (this.followState === "followed") ? "unfollowing" : "following";
+        this.render();
+
         _api_util__WEBPACK_IMPORTED_MODULE_0___default.a.toggleUser(this.userId, method)
             .then(
                 // by the code: 200 successful
                 (res) => {
-                    (this.followState === "followed") ? "unfollowed" : "followed";
+                    this.followState = (this.followState === "following") ? "followed" : "unfollowed";
                     this.render();
-                }
-            ).fail(
+                })
+            .fail(
                 // need a status code!
                 (err) => {
                     console.log('your code broke son');
--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -15,11 +15,19 @@ class FollowToggle {
     }
 
     render() {
+
         if (this.followState === 'unfollowed') {
             this.$el.text('Follow!');
-
-        } else {
+            this.$el.removeProp('disabled');
+        } else if (this.followState === "followed") {
             this.$el.text('Unfollow!');
+            this.$el.removeProp('disabled');
+        } else if (this.followState === "following") {
+            this.$el.text('following');
+            this.$el.prop('disabled', true)
+        } else if (this.followState === "unfollowing") {
+            this.$el.text('unfollowing');
+            this.$el.prop('disabled', true);
         }
     }
 
@@ -29,14 +37,17 @@ class FollowToggle {
         e.preventDefault();
         let method = (this.followState === "followed") ? 'DELETE' : 'POST';
 
+        this.followState = (this.followState === "followed") ? "unfollowing" : "following";
+        this.render();
+
         APIUtil.toggleUser(this.userId, method)
             .then(
                 // by the code: 200 successful
                 (res) => {
-                    (this.followState === "followed") ? "unfollowed" : "followed";
+                    this.followState = (this.followState === "following") ? "followed" : "unfollowed";
                     this.render();
-                }
-            ).fail(
+                })
+            .fail(
                 // need a status code!
                 (err) => {
                     console.log('your code broke son');
