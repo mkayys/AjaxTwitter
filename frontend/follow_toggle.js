0987071b770f95b8228354c1c5a22778b263f430
Michelle Kim <x1michelleskim@gmail.com>
2019-08-22 10:36:13 -0700

    Create constructor for follow toggle class

---
+++ frontend/follow_toggle.js
@@ -0,0 +1,10 @@
+class FollowToggle {
+    constructor($el) {
+        this.userId = $el.data('user-id');
+        this.followState = $el.data('initial-follow-state');
+
+        this.$el = $el;
+    }
+}
+
+export default FollowToggle;
