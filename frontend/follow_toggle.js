65eda2651543dc57c6de311b7c8c980d4a42d2eb
Michelle Kim <x1michelleskim@gmail.com>
2019-08-22 10:56:03 -0700

    Create a handleClick function to send an ajax call

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -11,11 +11,39 @@ class FollowToggle {
 
     render() {
         if (this.followState === 'unfollowed') {
-            this.$el.text('Follow');
+            this.$el.text('Follow!');
 
         } else {
-            this.$el.text('Unfollow');
+            this.$el.text('Unfollow!');
         }
+    }
+
+    handleClick(e) {
+        // handle something (naming convention for function) is just a callback to pass in the event listener
+
+        e.preventDefault();
+        let method = (this.followState === "followed") ? 'DELETE' : 'POST';
+
+        return $.ajax({
+            method: method,
+            url: `/users/${this.userId}/follow`,
+            dataType: 'JSON'
+        }).then(
+            // by the code: 200 successful
+            (res) => {
+                (this.followState === "followed") ? "unfollowed" : "followed";
+                this.render();
+            }
+        ).fail(
+            // need a status code!
+            (err) => {
+                console.log('your code broke son');
+            }
+        );
+
+
+
+
     }
 }
 
